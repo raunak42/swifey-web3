5862f96d283bf0b2e081629f564754a063cdf576
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-11-18 03:11:31 +0530

    WIll update.

---
+++ app/(tabs)/wallet.tsx
@@ -0,0 +1,8 @@
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+
+export default function Wallet(): JSX.Element {
+  return <ThemedView>
+    <ThemedText>Wallet</ThemedText>
+    </ThemedView>
+}
